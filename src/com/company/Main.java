8bf3e3e694ab07ee1520530e76e3e16728ae3232
HP-PC <maristaiu@yahoo.com>
2019-05-03 04:38:54 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+// Problema 18
+        public static void main(String[] args) {
+
+
+            char n = 'E';
+
+            if (n == 'N' || n == 'S' || n == 'E' || n == 'V') {
+                System.out.println(n + " este un punct cardinal");
+            } else {
+                System.out.println(n + " nu este un punt cardinal");
+            }
+        }
+    }
+
